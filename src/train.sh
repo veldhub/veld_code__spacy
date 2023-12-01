e433d16f2c7548216f59da0f94dbe54cc4a740f2
SteffRhes <stefanresch@protonmail.com>
2023-12-01 20:42:30 +0100

    new model, trained with data without empty entities

--- src/train.sh
+++ src/train.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 
-python -m spacy train /veld/executable/$config \
+python -m spacy train /veld/input/config.cfg \
   --paths.train /veld/input/train.spacy \
   --paths.dev /veld/input/dev.spacy \
   --output /veld/output/model/ \
   |& tee /veld/output/train.log
 
 python -m spacy evaluate /veld/output/model/model-best/ /veld/input/eval.spacy |& tee /veld/output/eval.log
+
