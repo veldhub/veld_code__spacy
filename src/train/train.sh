d6d4ffdbbb8422a1b8486431e3606c4ffda43538
SteffRhes <steffrhes@pm.me>
2024-06-05 22:39:33 +0200

    adapt code to newer veld metadata scheme and design

--- src/train/train.sh
+++ src/train/train.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-python -m spacy train /veld/input/config.cfg \
-  --paths.train /veld/input/train.spacy \
-  --paths.dev /veld/input/dev.spacy \
-  --output /veld/output/model/ \
-  |& tee /veld/output/train.log
+python -m spacy train /veld/code/config2.cfg \
+  --paths.train "$input_spacy_docbin_train" \
+  --paths.dev "$input_spacy_docbin_dev" \
+  --output "$output_model_path" \
+  |& tee "$output_train_log_path"
 
-python -m spacy evaluate /veld/output/model/model-best/ /veld/input/eval.spacy |& tee /veld/output/eval.log
+python -m spacy evaluate \
+  "${output_model_path}/model-best/" \
+  "$input_spacy_docbin_eval" \
+  |& tee "$output_eval_log_path"
 
