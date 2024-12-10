a9c43ec2f99e994586552284ef64b6e18b0ec9d0
SteffRhes <steffrhes@pm.me>
2024-12-10 10:21:46 +0100

    add condition into entrypoint regarding model_base var being not empty

--- src/load_models_base_cache.sh
+++ src/load_models_base_cache.sh
@@ -2,12 +2,15 @@
 #
 # this script is run as docker entrypoint, ensuring that spacy models are available and cached
 
-if [ -e /tmp/models_base_cache/"$model_base" ]; then
-  echo "found ${model_base} in /tmp/models_base_cache/ , using that."
-else
-  echo "could not find ${model_base} in /tmp/models_base_cache/ , downloading."
-  python -m spacy download "$model_base"
-  cp -r /usr/local/lib/python3.10/site-packages/"$model_base"* /tmp/models_base_cache/
+
+if [[ -n "$model_base" ]]; then
+  if [ -e /tmp/models_base_cache/"$model_base" ]; then
+    echo "found ${model_base} in /tmp/models_base_cache/ , using that."
+  else
+    echo "could not find ${model_base} in /tmp/models_base_cache/ , downloading."
+    python -m spacy download "$model_base"
+    cp -r /usr/local/lib/python3.10/site-packages/"$model_base"* /tmp/models_base_cache/
+  fi
 fi
 
 exec "$@"
