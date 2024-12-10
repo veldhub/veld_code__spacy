ccf6793632be6135d2bf5bd0e953572ce893f92b
SteffRhes <steffrhes@pm.me>
2024-12-10 10:41:17 +0100

    move dynamic PYTHONPATH modification entirely to entrypoint script

--- src/load_models_base_cache.sh
+++ src/load_models_base_cache.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #
-# this script is run as docker entrypoint, ensuring that spacy models are available and cached
-
+# this script is run as docker entrypoint, checking if $model_base var is set (indicating that spacy 
+# should base some workload on a pre-trained model provided by spacy). If this var is set, the 
+# $PYTHONPATH var is extended to the /tmp/models_base_cache/ volume where models are chached on the
+# host. This enables dynamic spacy model loading without needing to bake it into the docker image 
+# itself, while still avoiding downloads at each container start-up.
 
 if [[ -n "$model_base" ]]; then
+  export PYTHONPATH="/tmp/models_base_cache/:$PYTHONPATH"
   if [ -e /tmp/models_base_cache/"$model_base" ]; then
     echo "found ${model_base} in /tmp/models_base_cache/ , using that."
   else
